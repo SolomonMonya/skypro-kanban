fd8a27a37d51a27b95390ec349785201e553f1b5
SolomonMonya <admiral.aleks.d@gmail.com>
2024-02-28 18:08:21 +0300

    Step1Done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
+
+import useState from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import PopExit from './components/PopExit.jsx'
+import PopNewCard from './components/PopNewCard.jsx'
+import PopBrowse from './components/PopBrowse.jsx'
+import HeaderContainer from './components/HeaderContainer.jsx'
+import MainContainer from './components/MainContainer.jsx'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+	
+	return (
+		<>
+			<div className="wrapper">				
+				<PopExit />
+				<PopNewCard />
+				<PopBrowse />
+				<HeaderContainer />
+				<MainContainer />				
+			</div>
+			<script src="js/script.js"></script>
+		</>
+	)
 }
 
 export default App
+
+
